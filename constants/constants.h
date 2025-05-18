9407f984b286c4196b3aba8f3d2b4383b9c770d5
Stikarb <Stikarb@yandex.ru>
2025-05-18 21:11:16 +0300

    add constants.h

---
+++ constants/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define DIR_TEMPLATES "./templates/"
+#define DIR_CSS       "./css/"
+
+#define PATH_LOGIN_HTML     DIR_TEMPLATES "login.html"
+#define PATH_CONVERT_HTML   DIR_TEMPLATES "convert.html"
+#define PATH_RESULT_HTML    DIR_TEMPLATES "result.html"
+#define PATH_CSS_STYLES     DIR_CSS "styles.css"
+
+#define CONTENT_TYPE_HTML "Content-Type: text/html; charset=utf-8\n"
+#define CONTENT_TYPE_CSS  "Content-Type: text/css; charset=utf-8\n"
+
+#endif
